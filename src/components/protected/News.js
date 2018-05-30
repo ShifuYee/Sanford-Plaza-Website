fbceee70ee340f9b9071f58b2bff74f43f9f2657
Shifu Yee <shifuyee@github.com>
2018-05-30 13:23:15 -0400

    feat: add content and link to the News page

--- src/components/protected/News.js
+++ src/components/protected/News.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
-export default class Dashboard extends Component {
+import List from 'material-ui/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/InsertDriveFile';
+
+import NewsletterNov2016 from './content/Nov_2016_Newsletter.pdf';
+
+export default class News extends Component {
   render() {
     return (
       <div>
-        This is a protected route. You can only see this if you're authed.
+        <h1>Newsletters</h1>
+        
+        <List component="nav">
+          <ListItem>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <a href={NewsletterNov2016} target="_blank">
+              <ListItemText primary="November-2016-Newsletter" />
+            </a>
+          </ListItem>
+        </List>
       </div>
     );
   }
