4912a6fc00d6068563916b71ba5ba6a79bdb4bcb
Shifu Yee <shifuyee@github.com>
2018-05-30 13:51:26 -0400

    update: add a posting and notices section with documents

--- src/components/protected/Forms.js
+++ src/components/protected/Forms.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Forms extends Component {
   render() {
     return (
-      <div>
+      <div className="newpage">
         This is a protected route. You can only see this if you're authed.
       </div>
     );
